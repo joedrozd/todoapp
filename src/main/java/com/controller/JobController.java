2e4915d008f49b8f767508a89e29ec9d1e6fe786
Joe <Joe_drozd@yahoo.co.uk>
2022-02-01 10:01:12 +0000

    ◔_◔

--- src/main/java/com/controller/JobController.java
+++ src/main/java/com/controller/JobController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class JobController {
     @Autowired
     private JobService JobService;
-    private final Logger logger = LoggerFactory.getLogger(JobController.class);
+    private final Logger logger = LoggerFactory.getLogger(JobController.class);// ◔_◔ shh
     //POST
     @PostMapping("/addJob")
     public Job addJob(@RequestBody Job Job) {
