566e264933ea9489539444f7624ff6f1656edd61
Joe <Joe_drozd@yahoo.co.uk>
2022-02-01 13:45:07 +0000

    Update JobService.java

--- src/main/java/com/service/JobService.java
+++ src/main/java/com/service/JobService.java
@@ -5,14 +5,12 @@ import com.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class JobService {
     @Autowired
     private JobRepository JobRepository;
-    public List<Job> jobList = new ArrayList<>();
     
 
     //POST
