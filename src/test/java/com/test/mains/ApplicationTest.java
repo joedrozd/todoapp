3ac3ad7235344cf05bc774523de9f22fa8e1681e
Joe <Joe_drozd@yahoo.co.uk>
2022-02-02 13:58:16 +0000

    Test Set up

--- src/test/java/com/test/mains/ApplicationTest.java
+++ src/test/java/com/test/mains/ApplicationTest.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.test.mains;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoappApplicationTests {
+class ApplicationTest {
 
 	@Test
 	void contextLoads() {
