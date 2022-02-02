af630dda0f246ccbfab9a15fdf3914679ffe00ea
Joe <Joe_drozd@yahoo.co.uk>
2022-02-02 13:58:23 +0000

    Test Set up 2

---
+++ src/test/java/com/test/mains/controller/JobControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.test.mains.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JobControllerUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
